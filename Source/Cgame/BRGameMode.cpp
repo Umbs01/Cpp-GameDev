b14645dd4f64c2f3eec0bcc0280845e9f58d2cc7
PPman <66011072@kmitl.ac.th>
2024-02-17 17:09:40 +0700

    Added main menu & overall multiplayer update

---
+++ Source/Cgame/BRGameMode.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BRGameMode.h"
+
+ABRGameMode::ABRGameMode()
+{
+	// set default pawn class to our Blueprinted Basecharacter
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/BP_BaseCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
